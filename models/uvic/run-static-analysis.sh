a5c6c5532c32a7746eb7cd10aa519c1dffc321ae
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-03-20 22:09:04 +0100

    Updated analysis.

--- models/uvic/run-static-analysis.sh
+++ models/uvic/run-static-analysis.sh
@@ -18,59 +18,57 @@ else
         exit 1
 fi
 
-checkDirectory "Static data" "${STATIC_DATA_PATH}"
-checkDirectory "Source directory" "${SOURCE_CODE_PATH}"
-checkDirectory "Processed source directory" "${PROCESSED_CODE_PATH}"
-checkExecutable "fxtran" "${FXTRAN}"
+checkDirectory "Global data directory" "${GLOBAL_DATA_DIR}"
+
+STATIC_FILE_MODEL="${GLOBAL_DATA_DIR}/file"
+STATIC_MAP_MODEL="${GLOBAL_DATA_DIR}/map"
+STATIC_COMBINED_MODEL="${GLOBAL_DATA_DIR}/2-level"
+
+checkDirectory "Static file model" "${STATIC_FILE_MODEL}" recreate
+checkDirectory "Static map model" "${STATIC_MAP_MODEL}" recreate
+checkDirectory "Static 2-level model" "${STATIC_COMBINED_MODEL}" recreate
+
+checkExecutable "sar" "${SAR}"
+checkExecutable "maa" "${MAA}"
 
 # inputs
-KIEKER_LOG=${DYNAMIC_DATA_PATH}/`ls ${DYNAMIC_DATA_PATH}/ | grep kieker | grep $NAME`
-
 STATIC_CALL_LOG="${STATIC_DATA_PATH}/calltable.csv"
 STATIC_DATAFLOW_LOG="${STATIC_DATA_PATH}/dataflow.csv"
 
-STATIC_COMPONENT_MAP="${STATIC_DATA_PATH}/uvic-map-file.csv"
+STATIC_COMPONENT_MAP="${STATIC_DATA_PATH}/module-file-map.csv"
 GLOBAL_FUNCTION_MAP="${STATIC_DATA_PATH}/operation-definitions.csv"
 
 STATIC_LOG_CORRECTED="${STATIC_DATA_PATH}/MITgcm-$NAME/corrected-coupling.csv"
 STATIC_MODULE_MAP="${STATIC_DATA_PATH}/module-file-function-map.csv"
 
+information "Static architecture analysis - file based components"
 
-information "---------------------------------------------"
-information "Processing UVic: Experiment ${EXPERIMENT_NAME}"
-information "---------------------------------------------"
+INPUTS=""
+if [ -f "${STATIC_CALL_LOG}" ] && [ -f "${STATIC_DATAFLOW_LOG}" ] ; then
+	INPUTS="-i ${STATIC_CALL_LOG} -j ${STATIC_DATAFLOW_LOG}"
+elif [ -f "${STATIC_CALL_LOG}" ] ; then
+	INPUTS="-i ${STATIC_CALL_LOG}"
+elif [ -f "${STATIC_DATAFLOW_LOG}" ] ; then
+	INPUTS="-j ${STATIC_DATAFLOW_LOG}"
+else
+	error "No input logs for calls and dataflow available"
+	exit 1
+fi
 
-information "Create directory/file map"
-
-echo "module;file" > "${STATIC_CALL_LOG}"
-
-for EXT in f f90 f95 ; do
-	for I in `find "${SOURCE_CODE_PATH}" -iname "*.$EXT" -printf "%P\n"` ; do
-		echo $I	| sed 's/\/\([0-9A-Za-z\-_.]*\)/;\1/' >> "${STATIC_CALL_LOG}"
-	done
-done
-
-information "Parse processed code"
-
-CURRENT_PATH=`pwd`
-cd "${PROCESSED_CODE_PATH}"
-for I in `find . -name "*.f"` ; do
-	"${FXTRAN}" "$I"
-done
-cd "${CURRENT_PATH}"
-
-${FXCA} -i "${PROCESSED_CODE_PATH}" -o "${STATIC_DATA_PATH}"
-
-
-information "Static architecture analysis"
-
-echo "${SAR}" -i "${STATIC_CALL_LOG}" -j "${STATIC_DATAFLOW_LOG}" -c -E "${EXPERIMENT_NAME}" \
-	-f "${STATIC_FUNCTION_MAP}" "${ADDITIONAL_FUNCTION_MAP}" "${GLOBAL_FUNCTION_MAP}" \
+"${SAR}" ${INPUTS} -c -E "${EXPERIMENT_NAME}-file" \
+	-m file-mode \
 	-H "${HOST}" \
 	-o "${STATIC_FILE_MODEL}" -l static -ns ";" -ds ";" -cs ";"
-echo "${SAR}" -i "${STATIC_LOG}" -j "${STATIC_DATAFLOW_LOG}" -c -E "${EXPERIMENT_NAME}" \
-	-f "${STATIC_FUNCTION_MAP}" "${ADDITIONAL_FUNCTION_MAP}" "${GLOBAL_FUNCTION_MAP}" \
-	-H "${HOST}" -M "${STATIC_MODULE_MAP}" -m "${MISSING_FUNCTIONS_LIST}" \
-	-o "${STATIC_FILE_MODEL}" -l static -ns ";" -ds ";" -cs ";"
 
+information "Static architecture analysis - map based components"
 
+"${SAR}" ${INPUTS} -c -E "${EXPERIMENT_NAME}-map" \
+	-M "${STATIC_COMPONENT_MAP}"  -m map-mode \
+	-H "${HOST}" \
+	-o "${STATIC_MAP_MODEL}" -l static -ns ";" -ds ";" -cs ";"
+
+information "Combine map and file-based info"
+
+"${MAA}" -g "${STATIC_COMPONENT_MAP}" -i "${STATIC_FILE_MODEL}" -o "${STATIC_COMBINED_MODEL}" -gs ";"
+
+# end
