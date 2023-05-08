f77ec142bf7fc0856ea55bf269eb47d52a4d6003
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-05-08 10:07:14 +0200

    Improved compare models in mitgcm and extended configuration.

--- models/mitgcm/compare-models.sh
+++ models/mitgcm/compare-models.sh
@@ -11,12 +11,8 @@ else
         exit 1
 fi
 
-RESTRUCTURING="/home/reiner/temp/experiment/install/oceandsl-tools/bin/restructuring"
-OPTIMIZATION_DATA="/home/reiner/jss-results"
-
 checkExecutable "Restructuring" "${RESTRUCTURING}"
 checkDirectory "Result directory" "${OPTIMIZATION_DATA}"
-
 
 # main
 
@@ -25,52 +21,47 @@ jss-jobs-23-mitgcm_tutorial_barotropic_gyre_combined_map.job \
 jss-jobs-28-mitgcm_tutorial_barotropic_gyre_dynamic_iface-map.job \
 jss-jobs-29-mitgcm_tutorial_barotropic_gyre_dynamic_map.job \
 jss-jobs-34-mitgcm_tutorial_barotropic_gyre_static_iface-map.job \
-jss-jobs-35-mitgcm_tutorial_barotropic_gyre_static_map.job \
-jss-jobs-58-a-uvic_combined_iface-map-common.job \
-jss-jobs-58-b-uvic_combined_iface-map-embm.job \
-jss-jobs-58-c-uvic_combined_iface-map-mom.job \
-jss-jobs-59-a-uvic_combined_map-common.job \
-jss-jobs-59-b-uvic_combined_map-embm.job \
-jss-jobs-59-c-uvic_combined_map-mom.job \
-jss-jobs-64-a-uvic_dynamic_iface-map-common.job \
-jss-jobs-64-b-uvic_dynamic_iface-map-embm.job \
-jss-jobs-64-c-uvic_dynamic_iface-map-mom.job \
-jss-jobs-65-a-uvic_dynamic_map-common.job \
-jss-jobs-65-b-uvic_dynamic_map-embm.job \
-jss-jobs-65-c-uvic_dynamic_map-mom.job \
-jss-jobs-70-a-uvic_static_iface-map-common.job \
-jss-jobs-70-b-uvic_static_iface-map-embm.job \
-jss-jobs-70-c-uvic_static_iface-map-mom.job \
-jss-jobs-71-a-uvic_static_map-common.job \
-jss-jobs-71-b-uvic_static_map-embm.job \
-jss-jobs-71-c-uvic_static_map-mom.job ; do
+jss-jobs-35-mitgcm_tutorial_barotropic_gyre_static_map.job ; do
 
 	information "----------------------------------------"
 	information $I
 	information "----------------------------------------"
 
-	export ORIGINAL_ARCHIVE="${OPTIMIZATION_DATA}/$I/optimized-models.tar.xz"
-	export OPTIMIZED_ARCHIVE="${OPTIMIZATION_DATA}/$I/original-model.tar.xz"
+	export JOB_DIRECTORY="${OPTIMIZATION_DATA}/$I"
+
+	export ORIGINAL_ARCHIVE="${JOB_DIRECTORY}/optimized-models.tar.xz"
+	export OPTIMIZED_ARCHIVE="${JOB_DIRECTORY}/original-model.tar.xz"
+	export COMBINED_ARCHIVE="${JOB_DIRECTORY}/kieker-repositories.tar.xz"
+
+	cd "${JOB_DIRECTORY}"
 
 	if [ -f "${ORIGINAL_ARCHIVE}" ] && [ -f "${OPTIMIZED_ARCHIVE}" ] ; then
+		tar -xpf "${ORIGINAL_ARCHIVE}"
+		tar -xpf "${OPTIMIZED_ARCHIVE}"
+	elif [ -f "${COMBINED_ARCHIVE}" ] ; then
+		rm -rf "${JOB_DIRECTORY}/original-model"
+		for J in "${JOB_DIRECTORY}/optimized-"* ; do
+			if [ -d "$J" ] ; then
+				rm -rf "$J"
+			fi
+		done
+		tar -xpf "${COMBINED_ARCHIVE}"
+		mv "${JOB_DIRECTORY}/kieker-repositories/"* .
+	else
+		echo "No data."
+	fi
 
-		cd "${OPTIMIZATION_DATA}/$I"
+	ORIGINAL="${JOB_DIRECTORY}/original-model"
 
-		tar -xpf $ORIGINAL_ARCHIVE
-		tar -xpf $OPTIMIZED_ARCHIVE
-
-		ORIGINAL="${OPTIMIZATION_DATA}/$I/original-model"
-
+	if [ -d "${ORIGINAL}" ] ; then
 		LIST=""
 
-		for J in "${OPTIMIZATION_DATA}/$I/optimized-"* ; do
+		for J in "${JOB_DIRECTORY}/optimized-"* ; do
 			if [ -d "$J" ] ; then
 				LIST="$LIST $J"
 			fi
 		done
-		"${RESTRUCTURING}" -i "${ORIGINAL}" $LIST -o "${OPTIMIZATION_DATA}/$I" -e demo -s kuhn
-	else
-		echo "No data."
+		"${RESTRUCTURING}" -i "${ORIGINAL}" $LIST -o "${JOB_DIRECTORY}" -e demo -s kuhn
 	fi
 done
 
