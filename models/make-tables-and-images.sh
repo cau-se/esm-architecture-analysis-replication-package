780de5aa7d7055f7878592806c0d82ab3c9a1322
Reiner Jung <reiner.jung@email.uni-kiel.de>
2023-07-27 17:28:10 +0200

    Updated scripts.

--- models/make-tables-and-images.sh
+++ models/make-tables-and-images.sh
@@ -36,8 +36,8 @@ for LINE in `cat "${JOB_FILE}"` ; do
 			checkFile "diff file" "${TABLE_CSV}"
 			MERGE_MODEL="${JOB_DIRECTORY}/original-model-optimized-$OPT"
 			mkdir -p "${MERGE_MODEL}"
-			${MOP} -e merged-$OPT-$NAME  -i "${JOB_DIRECTORY}/original-model" "${JOB_DIRECTORY}/optimized-$OPT" -o "${MERGE_MODEL}" merge
-			
+			${MOP} -e merged-$OPT-$NAME  -i "${JOB_DIRECTORY}/original-model" "${JOB_DIRECTORY}/optimized-$OPT" -o "${MERGE_MODEL}" nearest-merge
+
 			LEFT=""
 			RIGHT="optimized-$OPT"
 			for E in `cat "${MERGE_MODEL}/source-model.xmi" | grep "<value>" | sort | uniq | sed 's/^ *<value>\(.*\)<\/value>$/\1/g' | grep -v "optimized-$OPT"` ; do
@@ -47,10 +47,16 @@ for LINE in `cat "${JOB_FILE}"` ; do
 					LEFT="$LEFT,$E"
 				fi
 			done
-			
-			${MVIS} -c allen num-of-calls op-coupling module-coupling -g dot-op dot-component -i "${MERGE_MODEL}" -o "${MERGE_MODEL}" -m add-nodes -s "all-color:$LEFT:$RIGHT"
-			${BASE_DIR}/dotPic-single-fileConverter.sh "${MERGE_MODEL}/original-model-optimized-$OPT-component.dot" pdf
-			${BASE_DIR}/dotPic-single-fileConverter.sh "${MERGE_MODEL}/original-model-optimized-$OPT-operation.dot" pdf
+
+			${MKTABLE} -i "${MERGE_MODEL}.csv" -o "${MERGE_MODEL}"
+			LAST_PWD=`pwd`
+			cd "${MERGE_MODEL}"
+			pdflatex full.tex
+			pdflatex compact.tex
+			cd $LAST_PWD
+			#${MVIS} -c allen num-of-calls op-coupling module-coupling -g dot-op dot-component -i "${MERGE_MODEL}" -o "${MERGE_MODEL}" -m add-nodes -s "all-color:$LEFT:$RIGHT"
+			#${BASE_DIR}/dotPic-single-fileConverter.sh "${MERGE_MODEL}/original-model-optimized-$OPT-component.dot" pdf &
+			#${BASE_DIR}/dotPic-single-fileConverter.sh "${MERGE_MODEL}/original-model-optimized-$OPT-operation.dot" pdf &
 		else
 			information $LINE
 			JOB_DIRECTORY="${OPTIMIZATION_DATA}/$LINE.job"
